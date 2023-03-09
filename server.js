fdfba3e9351cdddef4ffd3ca5e67b2a5e22a67ba
Volkov <info@viil.ru>
2023-03-10 02:53:01 +0300

    PORT FIXED

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 const indexFile = path.join(__dirname, '/dist/index.html');
 
 app.use(express.static('./dist/'));
